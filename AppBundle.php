0bfdfe8cf79298fa81c8958359eab12ae452ba07
Feft <piotrpikula@gmail.com>
2017-12-02 12:24:45 +0100

    add Symfony 3.4 framework

---
+++ AppBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AppBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Symfony 3.4 Bundle for learning and achievement test by using flash cards.
+ */
+class AppBundle extends Bundle
+{
+}
