506fbdee49c14cc1ea20480316ef040153263f5e
Feft <piotrpikula@gmail.com>
2017-12-03 20:41:59 +0100

    implementing addCardsToLearningBox and shuffleCards methods

--- Domain/LearningManager.php
+++ Domain/LearningManager.php
@@ -14,6 +14,15 @@ use Interfaces\LearningManagerInterface;
  */
 class LearningManager implements LearningManagerInterface
 {
+    /**
+     * @var array
+     */
+    private $learningBox;
+
+    public function __construct()
+    {
+        $this->learningBox = [];
+    }
 
     /**
      * Add cards to the learning box.
@@ -24,6 +33,8 @@ class LearningManager implements LearningManagerInterface
      */
     public function addCardsToLearningBox(array $cards): bool
     {
+        $this->learningBox = array_merge($this->learningBox, $cards);
+
         return true;
     }
 
@@ -32,6 +43,12 @@ class LearningManager implements LearningManagerInterface
      */
     public function shuffleCards()
     {
+        $i = 0;
+        do {
+            $array = $this->learningBox;
+            shuffle($this->learningBox);
+            $i++;
+        } while (count(array_intersect_assoc($array, $this->learningBox)) !== 0 || $i < 5);
 
     }
 
@@ -55,5 +72,18 @@ class LearningManager implements LearningManagerInterface
     public function moveCardToLearnedBox(FlashCard $card): bool
     {
         return true;
+    }
+
+    /**
+     * @return int size of learning box
+     */
+    public function countLearningBox(): int
+    {
+        return count($this->learningBox);
+    }
+
+    public function getLearningBox()
+    {
+        return $this->learningBox;
     }
 }
--- Tests/Domain/LearningManagerTest.php
+++ Tests/Domain/LearningManagerTest.php
@@ -15,4 +15,31 @@ class LearningManagerTest extends TestCase
         $this->assertInstanceOf(LearningManager::class, $learningManager);
         $this->assertInstanceOf(LearningManagerInterface::class, $learningManager);
     }
+
+    public function testAddingCardsToLearningBox()
+    {
+        $lm = new LearningManager();
+        $array1 = [0, 1, 2];
+        $lm->addCardsToLearningBox($array1);
+        $this->assertEquals(count($array1), $lm->countLearningBox());
+
+        $array2 = [0, 1, 2, 3, 4];
+        $lm->addCardsToLearningBox($array2);
+        $this->assertEquals(count($array1) + count($array2), $lm->countLearningBox());
+
+    }
+
+    /**
+     * Sometimes the shuffled array is identical
+     */
+    public function testShuffle()
+    {
+        $lm = new LearningManager();
+        $array1 = [0, 1, 2, 3];
+        $lm->addCardsToLearningBox($array1);
+        $lm->shuffleCards();
+        # if shuffled function array_intersect_assoc should return empty array
+        $this->assertEquals(0, count(array_intersect_assoc($array1, $lm->getLearningBox())));
+    }
+
 }
