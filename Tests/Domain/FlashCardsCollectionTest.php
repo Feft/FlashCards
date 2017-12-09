4e78abd934deb370660053de24b1d400efb3e8d9
Feft <piotrpikula@gmail.com>
2017-12-09 12:51:13 +0100

    unit tests for collection

--- Tests/Domain/FlashCardsCollectionTest.php
+++ Tests/Domain/FlashCardsCollectionTest.php
@@ -16,20 +16,50 @@ class FlashCardsCollectionTest extends TestCase
     {
         $collection = new FlashCardsCollection();
         for ($i = 0; $i < 4; $i++) {
-            $fc = new FlashCard();
-            $fc->setFlashCardsData((string)$i,(string)$i,$i);
-            $collection->addFlashCard($fc);
+            $collection->addFlashCard($this->prepareFlashCard((string)$i));
 
             $this->assertEquals($i + 1, $collection->getCollectionSize());
         }
 
-        $this->assertEquals(0,$collection->key());
+        $this->assertEquals(0, $collection->key());
         $collection->next();
-        $this->assertEquals(1,$collection->current()->getQuestion());
+        $this->assertEquals("q: 1", $collection->current()->getQuestion());
         $collection->next();
         $this->assertTrue($collection->valid());
         $collection->rewind();
-        $this->assertEquals(0,$collection->key());
+        $this->assertEquals(0, $collection->key());
+    }
+
+    public function testRemoveFirstElementFromCollection()
+    {
+        $collection = new FlashCardsCollection();
+        for ($i = 0; $i < 3; $i++) {
+            $collection->addFlashCard($this->prepareFlashCard((string)$i));
+        }
+        $element = $collection->removeFirstFlashCard();
+        $this->assertEquals(2, $collection->getCollectionSize());
+        $this->assertEquals("q: 0", $element->getQuestion());
+        $element = $collection->removeFirstFlashCard();
+        $this->assertEquals("q: 1", $element->getQuestion());
+        $element = $collection->removeFirstFlashCard();
+        $this->assertEquals("q: 2", $element->getQuestion());
+        # expected empty collection
+        $this->assertEquals(0, $collection->getCollectionSize());
+
+    }
+
+    /**
+     * Prepare card
+     *
+     * @param string $text text to set as question and answer
+     * @author PP
+     * @return FlashCard
+     */
+    private function prepareFlashCard(string $text): FlashCard
+    {
+        $fc = new FlashCard();
+        $fc->setFlashCardsData("q: " . $text, "a: " . $text, 1);
+        return $fc;
     }
 
 }
