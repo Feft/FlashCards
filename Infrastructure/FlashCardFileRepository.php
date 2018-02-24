20543d75679e78ca5cebd2e3736d7fd499356850
Feft <piotrpikula@gmail.com>
2018-02-24 19:30:29 +0100

    create storage for flashcards definitions

--- Infrastructure/FlashCardFileRepository.php
+++ Infrastructure/FlashCardFileRepository.php
@@ -6,8 +6,51 @@ use Interfaces\ObjectRepositoryInterface;
 
 class FlashCardFileRepository implements ObjectRepositoryInterface
 {
+    /**
+     * @var string 
+     */
+    private $storageFileName;
+
+    private $storageFolder;
+
+    private $storagePath;
+
+    public function __construct()
+    {
+        $this->storageFolder = 'Infrastructure/Storage';
+        $this->storageFileName = 'FlashCards.txt';
+        $this->storagePath = './'.$this->storageFolder.'/'.$this->storageFileName;
+
+
+        if($this->isFileStorageIsCreated() === false) {
+            $this->createFileStorage();
+        }
+    }
+
     public function findAll():array
     {
+        // $fileContent = file_get_contents($this->storageFileName);
+        // var_dump($fileContent);
+
         return [];
+    }
+
+    private function isFileStorageIsCreated()
+    {
+        if(file_exists($this->storagePath)) {
+            return true;
+        }
+        return false;
+    }
+
+    private function createFileStorage()
+    {
+        if (!file_exists('./'.$this->storageFolder)) {
+            mkdir('./'.$this->storageFolder, 0777, true);
+        }
+
+        $fileHandler = fopen($this->storagePath, 'w');
+        chmod($this->storagePath,0755);
+        fclose($fileHandler);
     }
 }
