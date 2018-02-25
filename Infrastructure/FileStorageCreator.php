08613bb283219f544c06963ec39e61454f00b0ed
Feft <piotrpikula@gmail.com>
2018-02-25 10:41:35 +0100

    storage creating in separate class

---
+++ Infrastructure/FileStorageCreator.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Infrastructure;
+
+/**
+ * Prepare simple storage in file
+ */
+class FileStorageCreator
+{
+    /**
+     * @var string
+     */
+    private $storageFileName;
+
+    private $storageFolder;
+
+    private $storagePath;
+
+    public function __construct()
+    {
+        $this->storageFolder = 'Infrastructure/Storage';
+        $this->storageFileName = 'FlashCards.txt';
+        $this->storagePath = './' . $this->storageFolder . '/' . $this->storageFileName;
+    }
+
+    public function configureFileStorage()
+    {
+        if ($this->isFileStorageIsCreated() === false) {
+            $this->createFileStorage();
+        }
+    }
+
+    public function getStoragePath()
+    {
+        return $this->storagePath;
+    }
+
+    private function isFileStorageIsCreated()
+    {
+        if (file_exists($this->storagePath)) {
+            return true;
+        }
+        return false;
+    }
+
+    private function createFileStorage()
+    {
+        if (!file_exists('./' . $this->storageFolder)) {
+            mkdir('./' . $this->storageFolder, 0777, true);
+        }
+
+        # temporary data
+        $flashCard[0]["question"] = "niegrzeczny (o dziecku)";
+        $flashCard[0]["answer"] = "naughty";
+        $flashCard[0]["difficultyLevel"] = 1;
+
+        $flashCard[1]["question"] = "niegrzeczny chłopiec";
+        $flashCard[1]["answer"] = "a naughty boy";
+        $flashCard[1]["difficultyLevel"] = 1;
+
+        $jsonData = json_encode($flashCard);
+
+        file_put_contents($this->storagePath, $jsonData, FILE_APPEND | LOCK_EX);
+        chmod($this->storagePath, 0755);
+    }
+}
--- Infrastructure/FlashCardFileRepository.php
+++ Infrastructure/FlashCardFileRepository.php
@@ -7,18 +7,11 @@ use Interfaces\ObjectRepositoryInterface;
 class FlashCardFileRepository implements ObjectRepositoryInterface
 {
     /**
-     * @var string
-     */
-    private $storageFileName;
-
-    private $storageFolder;
-
-    private $storagePath;
-
-    /**
      * Data stored in storage.
      */
     private $data;
+
+    private $storagePath;
 
     public function __construct()
     {
@@ -36,43 +29,10 @@ class FlashCardFileRepository implements ObjectRepositoryInterface
         return $this->data;
     }
 
-    private function isFileStorageIsCreated()
-    {
-        if (file_exists($this->storagePath)) {
-            return true;
-        }
-        return false;
-    }
-
-    private function createFileStorage()
-    {
-        if (!file_exists('./' . $this->storageFolder)) {
-            mkdir('./' . $this->storageFolder, 0777, true);
-        }
-
-        # temporary data
-        $flashCard[0]["question"] = "niegrzeczny (o dziecku)";
-        $flashCard[0]["answer"] = "naughty";
-        $flashCard[0]["difficultyLevel"] = 1;
-
-        $flashCard[1]["question"] = "niegrzeczny chłopiec";
-        $flashCard[1]["answer"] = "a naughty boy";
-        $flashCard[1]["difficultyLevel"] = 1;
-
-        $jsonData = json_encode($flashCard);
-
-        file_put_contents($this->storagePath, $jsonData, FILE_APPEND | LOCK_EX);
-        chmod($this->storagePath, 0755);
-    }
-
     private function configureFileStorage()
     {
-        $this->storageFolder = 'Infrastructure/Storage';
-        $this->storageFileName = 'FlashCards.txt';
-        $this->storagePath = './' . $this->storageFolder . '/' . $this->storageFileName;
-
-        if ($this->isFileStorageIsCreated() === false) {
-            $this->createFileStorage();
-        }
+        $storageCreator = new FileStorageCreator();
+        $storageCreator->configureFileStorage();
+        $this->storagePath = $storageCreator->getStoragePath();
     }
 }
