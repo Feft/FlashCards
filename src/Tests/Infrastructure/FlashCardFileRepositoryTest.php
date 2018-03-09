d768b532e161edf3d327ff474aad22af32595635
Feft <piotrpikula@gmail.com>
2018-03-09 18:33:07 +0100

    another unit test

--- src/Tests/Infrastructure/FlashCardFileRepositoryTest.php
+++ src/Tests/Infrastructure/FlashCardFileRepositoryTest.php
@@ -2,9 +2,9 @@
 
 namespace Feft\FlashCards\Tests\Infrastruture;
 
-use PHPUnit\Framework\TestCase;
 use Feft\FlashCards\Infrastructure\FileStorageCreator;
 use Feft\FlashCards\Infrastructure\FlashCardFileRepository;
+use PHPUnit\Framework\TestCase;
 
 class FlashCardFileRepositoryTest extends TestCase
 {
@@ -23,8 +23,16 @@ class FlashCardFileRepositoryTest extends TestCase
 
     public function testFindAllReturnsArrayIfLocalStorageIsCreated()
     {
-        // $mock = $this->getMockBuilder(FilesystemIterator::class)
-        //     ->setMethods(['isFileStorageIsCreated'])
-        //     ->getMock();
+        $stub = $this->getMockBuilder(FileStorageCreator::class)
+            ->enableOriginalConstructor()
+            ->setMethods(['isFileStorageIsCreated'])
+            ->getMock();
+
+        $stub->method('isFileStorageIsCreated')
+            ->willReturn(false);
+
+        $fileRepository = new FlashCardFileRepository($stub);
+        $result = $fileRepository->findAll();
+        $this->assertTrue(is_array($result));
     }
 }
